01c48d571583ec3c5642a27f68ff5c8963e8dcda
Angel Nuñez <snahider@gmail.com>
2019-10-06 18:19:21 -0500

    Tennis Ruby

---
+++ Tennis_Ruby/lib/tennis.rb
@@ -0,0 +1,7 @@
+class Tennis
+
+	def score
+		return "0-0"
+	end
+
+end
---
+++ Tennis_Ruby/spec/tennis_spec.rb
@@ -0,0 +1,8 @@
+require "tennis"
+
+describe Tennis do
+	it "dummy" do
+		tennis = Tennis.new
+		expect(tennis.score).to eq("0-0")
+	end
+end
